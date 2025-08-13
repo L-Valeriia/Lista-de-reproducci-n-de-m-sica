84b8ce36aaa058eebc4db04c24267c01df48445c
Valeria Zhumi <zhumivaleria@gmail.com>
2025-08-13 11:15:04 -0500

    actualiza el  playlist.js

--- playlist.js
+++ playlist.js
@@ -1,18 +1,46 @@
 let listaDeMusicas = [];
 
-function recirbirDatos(){
-  let titulo =  document.getElementById("titulo").value;
-  let artista =  document.getElementById("artista").value;
-  let duracion =  document.getElementById("duracion").value;
-  let genero =   document.getElementById("genero").value;
-  let cancion = {titulo, artista,duracion,genero}
-  return cancion;
+
+
+function agregarCancion() {
+  const nuevaCancion = {
+   titulo :  document.getElementById("titulo").value,
+   artista :  document.getElementById("artista").value,
+   duracion :  document.getElementById("duracion").value,
+   genero :  document.getElementById("genero").value,
+}
+  
+  if (!nuevaCancion.titulo || !nuevaCancion.artista || !nuevaCancion.duracion || !nuevaCancion.genero) {
+  alert("Por favor, completa todos los campos antes de agregar la canción.");
+  }else{
+  listaDeMusicas.push(nuevaCancion);
+  alert("La nueva cancion ha sido agregada")
+  console.log("Lista actual:", listaDeMusicas);
+  ["titulo", "artista", "duracion", "genero"].forEach(id => {
+      document.getElementById(id).value = "";
+  })}
+}
+
+function cancionesAñadidas() {
+  const contenedor = document.getElementById("listaCompleta");
+  contenedor.innerHTML = ""; 
+
+  if (listaDeMusicas.length === 0) {
+    contenedor.textContent = "No hay canciones agregadas.";
+    return;
+  }
+
+  const lista = document.createElement("ul");
+
+  listaDeMusicas.forEach(cancion => {
+    const titulo = document.createElement("h3");
+  titulo.textContent = `Lista de canciones: ${listaDeMusicas.length}`;
+  contenedor.appendChild(titulo);
+    const li = document.createElement("li");
+    li.textContent = `${cancion.titulo} - ${cancion.artista} (${cancion.duracion} min) [${cancion.genero}]`;
+    lista.appendChild(li);})
+  contenedor.appendChild(lista);
 }
 
 
-
-function agregarCancion () {
-    let cancionNueva =
-    listaDeMusicas.push(recirbirDatos())
-    return ;
-}
+  
